4354cc3caa9a9a959fe9c587acc7b5633a05c0bb
billie11111 <jyjcy501@naver.com>
2023-12-03 16:55:03 +0900

    first commit

--- app/scroll-test/animation.ts
+++ app/scroll-test/animation.ts
@@ -8,3 +8,28 @@ export const fadeInAnimationsVariants = {
     y: "0px",
   },
 };
+
+export const svgVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 3,
+    },
+  },
+};
+
+export const pathVariants = {
+  initial: {
+    pathLength: 0,
+  },
+  animate: {
+    pathLength: 1,
+    transition: {
+      duration: 3,
+      ease: "easeInOut",
+    },
+  },
+};
--- app/scroll-test/page.tsx
+++ app/scroll-test/page.tsx
@@ -1,131 +1,76 @@
 'use client'
-import { motion, useTransform, Variants } from "framer-motion"
+import { motion, useScroll, useTransform, Variants } from "framer-motion"
 import { useEffect, useRef } from "react";
 import { styled } from "styled-components";
-import { fadeInAnimationsVariants } from "./animation";
-import Home from "./components/home";
-
-
+import { fadeInAnimationsVariants, pathVariants, svgVariants } from "./animation";
+import InterviewSVG from "./components/interviewSVG";
+import IntroSVG from "./components/introSVG";
+import VisionSVG from "./components/visionSVG";
 
 //Styles
 const Wrapper = styled(motion.section)`
   background-color: #333;
-  height: 100vh;
+  height: auto;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 6vw;
   color: white;
   font-weight: bold;
-  border-bottom: 1px solid red;
 
   span {
     display:block;
   }
 `;
 
-//Component
+const IntroContainer = styled.div`
+  min-width: 1440px;
+  margin: calc(80px + 120px) auto auto;
+  padding: 140px 0 0;
+  //border: 1px solid red;
+`;
 
+const Container = styled.div`
+  min-width: 1440px;
+  margin: auto;
+  /* border: 1px solid red; */
 
+  .interview-svg {
+    width: 97%;
+    position: relative;
+    top: -2px;
+  }
+`;
 
 //Page
 export default function ScrollTestPage() {
 
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start center", "end end"]
+  });
 
   return (
     <motion.main initial="hidden" animate="show">
 
       {/* Section_01 */}
       <Wrapper>
-        <h1>
-          <motion.span
-            variants={fadeInAnimationsVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: false,
-              amount: 1,
-            }}
-            transition={{
-              bounce: 0,
-              duration: 0.5,
-              delay: 0.5
-            }}
-          >
-            Title
-          </motion.span>
-        </h1>
+        <IntroContainer>
+          <IntroSVG svgVariants={svgVariants} pathVariants={pathVariants} />
+        </IntroContainer>
       </Wrapper>
 
       {/* Section_02 */}
-      <Wrapper>
-        <h1>
-          <motion.span
-            variants={fadeInAnimationsVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: false,
-              amount: 1,
-            }}
-            transition={{
-              bounce: 0,
-              duration: 0.5,
-              delay: 0.5
-            }}
-          >
-            Title
-          </motion.span>
-        </h1>
+      <Wrapper ref={ref}>
+        <Container>
+          <InterviewSVG scrollYProgress={scrollYProgress} />
+        </Container>
       </Wrapper>
+
+
 
       {/* Section_03 */}
-      <Wrapper>
-        <h1>
-          <motion.span
-            variants={fadeInAnimationsVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: false,
-              amount: 1,
-            }}
-            transition={{
-              bounce: 0,
-              duration: 0.5,
-              delay: 0.5
-            }}
-          >
-            Title
-          </motion.span>
-        </h1>
-      </Wrapper>
-
-      {/* Section_04 */}
-      <Wrapper>
-        <h1>
-          <motion.span
-            variants={fadeInAnimationsVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-              once: false,
-              amount: 1,
-            }}
-            transition={{
-              bounce: 0,
-              duration: 0.5,
-              delay: 0.5
-            }}
-          >
-            Title
-          </motion.span>
-        </h1>
-      </Wrapper>
-
-
-
-
     </motion.main>
   )
 }
