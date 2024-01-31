62b4b4879893394ec5b02ef73084c5f530b05d41
billie11111 <jyjcy501@naver.com>
2024-01-31 20:43:57 +0900

    aaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useRef } from "react";
 import { styled } from "styled-components";
 import ErrorBarChart from "./components/errorBarChart";
@@ -10,7 +10,7 @@ import SemiDonutGaugeChart from "./components/semiDonutGaugeChart";
 import { TableChart } from "./components/tableChart";
 
 const Wrapper = styled.main`
-  overflow:hidden;
+  overflow: hidden;
   max-width: 450px;
   padding: 25px 30px 25px 30px;
   width: 100%;
@@ -26,7 +26,7 @@ const Wrapper = styled.main`
   }
 
   .error-container {
-    display:flex;
+    display: flex;
     justify-content: center;
     align-items: center;
   }
@@ -38,7 +38,7 @@ const SemiDonutGaugeWrapper = styled.main`
   max-height: 320px;
   height: 100%;
   padding: 25px 30px 25px 30px;
-  overflow:hidden;
+  overflow: hidden;
   margin: 5vh auto 0 auto;
   box-sizing: border-box;
   border: 1px solid #e1e1e1;
@@ -46,18 +46,66 @@ const SemiDonutGaugeWrapper = styled.main`
   box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.08);
 `;
 
-const data = {
-  organization: 'high', //조직화
-  elaboration: 'high', //정교화
-}
+const ScrollWrapper = styled.div`
+  overflow: hidden;
+  max-width: 450px;
+  padding: 25px 30px 25px 30px;
+  width: 100%;
+  max-height: 90%;
+  height: auto;
+  margin: 5vh auto 0 auto;
+  box-sizing: border-box;
+  border: 1px solid #e1e1e1;
+  border-radius: 10px;
+  box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.08);
 
+  & > .container {
+    min-height: 100%;
+    height: 67rem;
+    overflow-y: scroll;
+    padding-bottom: 5rem;
+
+    &::-webkit-scrollbar {
+      width: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background: rgba(220, 20, 60); /* 스크롤바 색상 */
+      border-radius: 10px; /* 스크롤바 둥근 테두리 */
+    }
+
+    &::-webkit-scrollbar-track {
+      background: transparent; /*스크롤바 뒷 배경 색상*/
+    }
+  }
+`;
+
+const data = {
+  organization: "high", //조직화
+  elaboration: "high", //정교화
+};
 
 export default function Home() {
-
   const tbodyRef = useRef(null);
 
   return (
     <>
+      <ScrollWrapper>
+        <div className="container">
+          <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart />
+          {/* <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart />
+          <ErrorBarChart /> */}
+        </div>
+      </ScrollWrapper>
       <Wrapper>
         <h2>에러바 차트</h2>
         <ErrorBarChart />
@@ -69,21 +117,21 @@ export default function Home() {
           title="문제해결중심"
           value={5}
           average={3}
-          chartColor={['#D8C0FF', '#7A69DF']}
+          chartColor={["#D8C0FF", "#7A69DF"]}
         />
         <HorizontalBarChart
           chartId="chart-2"
           title="감정해소중심"
           value={3.2}
           average={3}
-          chartColor={['#FFC5AB', '#FF7D59']}
+          chartColor={["#FFC5AB", "#FF7D59"]}
         />
         <HorizontalBarChart
           chartId="chart-3"
           title="도구적해결중심"
           value={2}
           average={2.4}
-          chartColor={['#D8F1CE', '#7DC387']}
+          chartColor={["#D8F1CE", "#7DC387"]}
         />
       </Wrapper>
       <Wrapper>
@@ -106,5 +154,5 @@ export default function Home() {
         <TableChart ref={tbodyRef} data={data} />
       </Wrapper>
     </>
-  )
+  );
 }
